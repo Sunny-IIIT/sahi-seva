29e7a56c2e8ee257a6321202a9fdafe133b25b3c
Sunny <sunnyk31728@gmail.com>
2026-08-28 01:12:17 +0530

    chore: Revert Demo Section injection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ export const dynamic = 'force-dynamic';
 import { Hero } from "@/components/Hero";
 import { CategoryGrid } from "@/components/CategoryGrid";
 import { FeaturedWorkers } from "@/components/FeaturedWorkers";
-import { SIHDemoSection } from "@/components/SIHDemoSection";
 
 export default function Home() {
   return (
     <>
-      <SIHDemoSection />
       <Hero />
       <FeaturedWorkers />
       <CategoryGrid />
